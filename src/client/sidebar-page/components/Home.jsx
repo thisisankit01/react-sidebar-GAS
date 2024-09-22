25058e5cb2a9b1dfc673e61ab886b49f4e733b78
thisisankit01 <ankitechzz@gmail.com>
2024-09-23 01:45:29 +0530

    page redirection working after succesful  callback

--- src/client/sidebar-page/components/Home.jsx
+++ src/client/sidebar-page/components/Home.jsx
@@ -1,15 +1,43 @@
 /* eslint-disable prettier/prettier */
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const handleConnectToQuickBooks = () => {
     const authRoute = `http://localhost:3000/auth`;
     window.open(authRoute, '_blank', 'width=500,height=600');
   };
 
+  useEffect(() => {
+    const handleMessage = (event) => {
+      if (event.data === 'authSuccess') {
+        setIsAuthenticated(true);
+        console.log('Authentication successful!');
+        alert('Successfully authenticated with QuickBooks!');
+      } else if (event.data === 'authFailure') {
+        setIsAuthenticated(false);
+        console.log('Authentication failed.');
+        alert('Authentication failed. Please try again.');
+      }
+    };
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Connect to QuickBooks</h1>
-      <button onClick={handleConnectToQuickBooks}>Connect</button>
+      {isAuthenticated ? (
+        <h1>User is Authenticated Successfully</h1>
+      ) : (
+        <div>
+          <h1>Connect to QuickBooks</h1>
+          <button onClick={handleConnectToQuickBooks}>Connect</button>
+        </div>
+      )}
     </div>
   );
 };
